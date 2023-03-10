879f98da7f1254b6c41b33c41240cd5a9fdc7cef
Alessandro Nazzari <alessandro.nazzari@gmail.com>
2023-03-10 10:15:41 +0100

    First example

--- examples/tensorflow2/as_a_function/README.md
+++ examples/tensorflow2/as_a_function/README.md
@@ -1,18 +1,38 @@
 
-Here we use Keras backend functions to split the execution of the model into two submodels. While this approach is trivial in case of linear models particular attention must be given to models with skip connections. As an example we show a small U-Net model ![](unet.png) <br>
-If we want to inject an error into the third convolutional layer we need to store the output of the second convolution in order to concate it and produce the final output.
-Our injection function will thus be like this 
+Here we use Keras backend functions to split the execution of the model into two submodels. While this approach is trivial in case of linear models particular attention must be given to models with skip connections. 
+## U-Net
+
+As an example we show a small U-Net model ![](unet.png) <br>
+If we want to inject an error into the third convolutional layer we need to do the following
+1. Execute the model from the input layer through the targeted convolutional layer
+2. Modify the output of the convolutional layer based on the selected injection sites
+3. Execute the model from the convolutional layer through the concatenate layer
+4. Calculate the second input of the concatenate layer
+5. Execute the model from the concatenate layer through the output layer
+
+To do so we need to use the following functions>
 ```python
 get_selected_layer_output = K.function([model.layers[0].input], [model.layers[selected_layer_idx].output])
 get_conv2_output = K.function([model.layers[0].input], [model.layers[conv2_idx].output])
 get_input_concatenate = K.function([model.layers[selected_layer_idx + 1].input], [model.layers[conv2d_transpose_idx].output])
 get_final_output = K.function([model.layers[concatenate_idx].input], [model.layers[-1].output])
 ```
-The functions produce the following results
+They respectively produce the following results
 
-1. `get_selected_layer_output`: produces the output of the selected layer
-2. `get_conv2_output`: produces the output of the second convolutional layer, the ones that must be given to the concatenate layer
-3. `get_input_concatenate`: produces the output of the conv2d_transpose layer given the corrupted output of our target convolutional layer
-4. `get_final_output`: produces the final output of the model starting from the concatenate layer
+1. `get_selected_layer_output`: output of the selected layer
+2. `get_conv2_output`: output of the second convolutional layer, the first input of the concatenate layer
+3. `get_input_concatenate`: output of the conv2d_transpose layer, second input of the concatenate layer
+4. `get_final_output`:  final output of the model starting from the concatenate layer
 
-This process is clearly time consuming and does not easily scale with very complex models, for this reason we suggest to use this version of the error simulator with linear models and models with few skip connections while preferring the layer adaptation of the framework for more complex models.
+## Linear model
+If we have a model with no skip connections ![](linear.png)
+the process is straightforward. 
+As an example let's suppose we want to target the second convolutional layer, the only functions we need are the following 
+```python
+get_selected_layer_output = K.function([model.layers[0].input], [model.layers[selected_layer_idx].output])
+get_output_injected = K.function([model.layers[selected_layer_idx + 1].input], [model.layers[-1].output])
+```
+The first one produces the output of the selected layer, we can then corrupt it and pass it to the second function that produces the final output of the model.
+
+## Examples
+`lenet.py` and `small_unet.py` are fully documented examples.
---
+++ examples/tensorflow2/as_a_function/linear.py
@@ -0,0 +1,120 @@
+import tensorflow as tf
+from tensorflow import keras
+from keras import datasets, layers, models, losses
+from keras import backend as K
+import numpy as np
+
+import sys
+sys.path.insert(1, '../src')
+
+from injection_sites_generator import *
+from fault_injector_keras import *
+
+
+### PERFORMING INJECTIONS ON A LINEAR MODEL
+#
+# To correctly perform injections on a linear model we first have to select the layer we want to inject.
+# We can iterate through model.layers to find the corresponding index and store it (line 109)
+# Then we define the number of tests that we want to perform (line 107)
+# and execute the function inject_layer each time (lines 115 - 116)
+# We need to pass the following parameters
+# 		1. model: the model we are targeting
+#		2. img: an image on which we will perform inference
+#		3. selected layer index: the index referring to the layer we selected for the injection
+#		4. operator type: a value from the OperatorType enum that defines the type of the layer we are injecting
+#		5. shape: the output shape of the layer as a string in the following format: (None, channels, widht, height)
+#
+# The function will return the corrupted output of the model
+
+def generate_injection_sites(sites_count, layer_type, layer_name, size, models_mode=''):
+
+	injection_site = InjectableSite(layer_type, layer_name, size)
+
+	try:
+		injection_sites, cardinality, pattern = InjectionSitesGenerator([injection_site], models_mode) \
+			.generate_random_injection_sites(sites_count)
+	except:
+		return []
+
+	return injection_sites, cardinality, pattern
+
+
+def build_model(input_shape, saved_weights=None):
+	'''
+	Saved weights should be the path to a h5 file if you have already trained the model
+	'''
+	if saved_weights is not None:
+		model = keras.models.load_model(saved_weights)
+		return model
+	inputs = keras.Input(shape=input_shape, name='input')
+	conv1 = layers.Conv2D(filters=6, kernel_size=(5, 5), activation='relu', name='conv1')(inputs)
+	pool1 = layers.MaxPool2D(pool_size=(2, 2), strides=(1, 1), name='maxpool1')(conv1)
+	conv2 = layers.Conv2D(filters=16, kernel_size=(5, 5), strides=(1, 1), activation='relu', padding="same",
+						  name='conv2')(pool1)
+	pool2 = layers.MaxPool2D(pool_size=(2, 2), strides=(1, 1), name='maxpool2')(conv2)
+	conv3 = layers.Conv2D(filters=120, kernel_size=(5, 5), strides=(1, 1), activation='relu', padding="same",
+						  name='conv3')(pool2)
+	flatten = layers.Flatten(name='flatten')(conv3)
+	dense1 = layers.Dense(84, activation='relu', name='dense1')(flatten)
+	outputs = layers.Dense(10, activation='softmax', name='dense3')(dense1)
+
+	return keras.Model(inputs=(inputs,), outputs=outputs)
+
+def load_data():
+	(x_train,y_train),(x_test,y_test) = datasets.mnist.load_data()
+	x_train = tf.pad(x_train, [[0, 0], [2,2], [2,2]])/255
+	x_test = tf.pad(x_test, [[0, 0], [2,2], [2,2]])/255
+	x_train = tf.expand_dims(x_train, axis=3, name=None)
+	x_test = tf.expand_dims(x_test, axis=3, name=None)
+
+	x_val = x_train[-2000:,:,:,:] 
+	y_val = y_train[-2000:] 
+	x_train = x_train[:-2000,:,:,:] 
+	y_train = y_train[:-2000]
+
+	return x_train, y_train, x_val, y_val
+
+
+def train_model(model, x_train, y_train, x_val, y_val, batch_size, epochs, save_path):
+	model.compile(optimizer='adam', loss=losses.sparse_categorical_crossentropy, metrics=['accuracy'])
+	history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_val, y_val))
+	keras.models.save_model(model, save_path)
+
+
+def inject_layer(model, img, selected_layer_idx, injection_model, layer_output_shape_cf):
+	get_selected_layer_output = K.function([model.layers[0].input], [model.layers[selected_layer_idx].output])
+	get_model_output = K.function([model.layers[selected_layer_idx + 1].input], [model.layers[-1].output])
+
+
+	output_selected_layer = get_selected_layer_output([np.expand_dims(img, 0)])[0]
+
+	injection_site, cardinality, pattern = generate_injection_sites(1, injection_model, '',
+																		layer_output_shape_cf)
+
+	if len(injection_site) > 0:
+		for idx, value in injection_site[0].get_indexes_values():
+			channel_last_idx = (idx[0], idx[2], idx[3], idx[1])
+			if value.value_type == '[-1,1]':
+				output_selected_layer[channel_last_idx] += value.raw_value
+			else:
+				output_selected_layer[channel_last_idx] = value.raw_value
+	
+	model_output = get_model_output(output_selected_layer)
+
+	return model_output 
+
+
+NUM_INJECTIONS = 100
+NUM = 42
+SELECTED_LAYER_IDX = 3
+
+x_train, y_train, x_val, y_val = load_data()
+model = build_model(x_train[0].shape, saved_weights='weights.h5')
+errors = 0
+
+for _ in range(NUM_INJECTIONS):
+	res = inject_layer(model, x_val[NUM], SELECTED_LAYER_IDX, OperatorType['Conv2D'], '(None, 16, 27, 27)')
+	if np.argmax(res) != y_val[NUM]:
+		errors += 1
+
+print(f'Number of misclassification over {NUM_INJECTIONS} injections: {errors}')
